28f80b2436d85c4afa8a9c9e3a2f2587c4e31968
Surajit <surajitjana20107@gmail.com>
2025-12-06 07:08:41 +0000

    add navbar component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,28 @@
+import { Menu, Search, Github } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <nav className="bg-[#212137]">
-      Header
+    <nav className="h-16 w-full flex p-4 items-center justify-between bg-zinc-600">
+      <Menu />
+      <div className="relative h-8 w-36">
+        <Link href="/">
+          <Image
+            src="/gma-dark.svg"
+            alt="Logo"
+            fill
+            priority
+            className="object-contain"
+            />
+        </Link>
+      </div>
+      <div className="flex items-center justify-center h-full w-auto gap-4">
+        <Search />
+        <a href="https://github.com/surajit20107" target="_blank">
+          <Github />
+        </a>
+      </div>
     </nav>
   )
 }
