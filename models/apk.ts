b156296c9629f4af6b06ede5067d231fa8aca066
Surajit <surajitjana20107@gmail.com>
2025-12-09 05:15:50 +0000

    add apk mongoose schema

---
+++ models/apk.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const apkSchema = new Schema({
+  name: String,
+  image: String,
+  packageName: String,
+  publisher: String,
+  category: String,
+  size: String,
+  rating: Number,
+  version: String,
+  platform: String,
+  price: String,
+  description: String,
+  downloadUrl: String,
+  requirements: String,
+  screenshots: [String],
+  modInfo: String,
+}, {timestamps: true})
+
+export default mongoose.models.Apk || model("Apk", apkSchema);
