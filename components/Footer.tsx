a5baf8c7b9324530452bb41972ba1b51e25ceb42
Surajit <surajitjana20107@gmail.com>
2025-12-09 04:19:18 +0000

    add footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,6 +24,15 @@ export default function Footer() {
           . All Rights Reserved.
         </p>
       </div>
+      <div className="footer-links text-gray-500 px-2 flex flex-wrap gap-4 py-4 items-center justify-center">
+        <p>Privacy Policy</p>
+        <p>About Us</p>
+        <p>Contact Us</p>
+        <p>DMCA Disclaimer</p>
+        <p>Terms of Service</p>
+        <p>Sitemap</p>
+      </div>
+      <hr className="border-gray-700" />
       <div className="container mx-auto py-2">
         <p className="text-center text-gray-400">
           Made with ❤️ & lots of ☕ by{" "}
