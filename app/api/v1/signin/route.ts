aa82550de335e56a1ce223a448c3019da1cfaa16
Surajit <surajitjana20107@gmail.com>
2025-12-09 16:24:09 +0000

    user login api complete

---
+++ app/api/v1/signin/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/user";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+
+    const fields = [email, password];
+
+    const invalidFields = fields.some(
+      (field) => typeof field !== "string" || field.trim() === "",
+    );
+
+    if (invalidFields) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 },
+      );
+    }
+
+    await connectToDatabase();
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "Invalid credentials" },
+        { status: 401 },
+      );
+    }
+
+    const isCorrectPassword = await user.comparePassword(password);
+
+    if (!isCorrectPassword) {
+      return NextResponse.json(
+        { message: "Invalid credentials" },
+        { status: 401 },
+      );
+    }
+
+    const token = user.generateToken();
+
+    const response = NextResponse.json(
+      { message: "Sign in successfully" },
+      { status: 200 },
+    );
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while signing in" },
+      { status: 500 },
+    );
+  }
+}
