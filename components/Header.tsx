55b9e8f1f1ce962442cc8dbcdaa9fd53dc89f020
Surajit <surajitjana20107@gmail.com>
2026-01-11 14:58:44 +0000

    add search bar ui in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { Menu, Search, Github } from "lucide-react";
+import { useState } from "react";
+import { Menu, Search, Github, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,27 +8,50 @@ interface HeaderProps {
   onMenuClick: () => void;
 }
 
-export default function Header({onMenuClick}: HeaderProps) {
+export default function Header({ onMenuClick }: HeaderProps) {
+  const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
+  const [keyword, setKeyword] = useState<string>("");
+
   return (
-    <nav className="h-16 w-full flex p-4 items-center justify-between bg-zinc-600">
+    <nav className="h-16 w-full flex p-4 items-center bg-zinc-600">
       <Menu onClick={onMenuClick} />
-      <div className="relative h-8 w-36">
-        <Link href="/">
-          <Image
-            src="/gma-dark.svg"
-            alt="Logo"
-            fill
-            priority
-            className="object-contain"
-            />
-        </Link>
+      <div className="flex flex-1 items-center justify-center px-4">
+        {!isSearchOpen && (
+          <div className="relative h-8 w-36">
+            <Link href="/">
+              <Image
+                src="/gma-dark.svg"
+                alt="Logo"
+                fill
+                priority
+                className="object-contain"
+              />
+            </Link>
+          </div>
+        )}
+
+        {isSearchOpen && (
+          <input
+            type="text"
+            placeholder="Search by app names..."
+            className="w-full bg-[#1F2937] text-white rounded-xl py-2 px-3 outline-none ease-in-out duration-300"
+            value={keyword.toString()}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+        )}
       </div>
+
       <div className="flex items-center justify-center h-full w-auto gap-4">
-        <Search />
+        {isSearchOpen ? (
+          <X onClick={() => setIsSearchOpen(false)} />
+        ) : (
+          <Search onClick={() => setIsSearchOpen(true)} />
+        )}
+
         <a href="https://github.com/surajit20107" target="_blank">
           <Github />
         </a>
       </div>
     </nav>
-  )
+  );
 }
