95c02ef2eebffc357b064f6e94482b51fa596fb8
Surajit <surajitjana20107@gmail.com>
2026-01-11 04:29:58 +0000

    create search api for find apps by searching name

---
+++ app/api/v1/apk/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import Apk from "@/models/apk";
+
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const search = searchParams.get("search");
+    
+    await connectToDatabase();
+    
+    const apks = await Apk.find({ name: { $regex: search, $options: "i" }})
+
+    return NextResponse.json(
+      { apks },
+      { status: 200 });
+
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Internal server error, try again later" },
+      { status: 500 }
+    )
+  }
+}
